1fd35e66afd520ec6e83a4ab57dd5babf6db6ddf
Łukasz Sypniewski <lukasz.sypniewski@guestline.com>
2021-10-21 02:05:26 +0200

    Add PROJECT_SUBFOLDER parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC2153,2164
 
 set -o pipefail
 
@@ -12,6 +13,7 @@ sourceUrl=${SOURCE_URL:-https://nuget.pkg.github.com/"$repo_owner"/index.json}
 sourceUsername=${SOURCE_USERNAME}
 sourcePassword=${SOURCE_PASSWORD}
 apiKey=${API_KEY}
+project_subfolder=${PROJECT_SUBFOLDER}
 
 
 # Print config
@@ -21,33 +23,41 @@ echo -e "\tPROJECT_NAME: ${project_name}"
 echo -e "\tSOURCE_NAME: ${sourceName}"
 echo -e "\tSOURCE_URL: ${sourceUrl}"
 echo -e "\tSOURCE_USERNAME: ${sourceUsername}"
+echo -e "\tPROJECT_SUBFOLDER: ${project_subfolder}"
 
 echo "Preparing NuGet package of $project_name project"
 
 
 cd /github/workspace
 
-if [ -z "$sourceUsername"] || [ -z "$sourcePassword" ]; then
+if [ -z "$sourceUsername" ] || [ -z "$sourcePassword" ]; then
     echo "Source username or password is empty - they will not be set in nugetconfig"
-    dotnet nuget add source ${sourceUrl} -n ${sourceName}
+    dotnet nuget add source "${sourceUrl}" -n "${sourceName}"
 else 
     echo "Source username and password are not empty - they will be set in nugetconfig"
-    dotnet nuget add source ${sourceUrl} -n ${sourceName} -u ${sourceUsername} -p ${sourcePassword} --store-password-in-clear-text
+    dotnet nuget add source "${sourceUrl}" -n "${sourceName}" -u "${sourceUsername}" -p "${sourcePassword}" --store-password-in-clear-text
 fi
 
 #Create package 
 
-dotnet pack "${project_name}/${project_name}.csproj" -c Release -p:RepositoryCommit="${commit_id}" -p:Version=${version} --output nuget-packages/"${project_name}" --include-symbols 
+CSPROJ_PATH="${project_name}/${project_name}.csproj"
+
+if [ -n "$project_subfolder" ]; then
+CSPROJ_PATH="${project_name}/$project_subfolder/${project_name}.csproj"
+fi
+
+
+dotnet pack "$CSPROJ_PATH" -c Release -p:RepositoryCommit="${commit_id}" -p:Version="${version}" --output nuget-packages/"${project_name}" --include-symbols 
 
 
 #Publish package
 echo "Publishing NuGet package of $project_name project"
 
-if [ -z "$apiKey"]; then
+if [ -z "$apiKey" ]; then
     echo "API key has not been provided, it will not be used for 'nuget push'"
-    dotnet nuget push nuget-packages/"${project_name}"/*.nupkg --source ${sourceName} --skip-duplicate
+    dotnet nuget push nuget-packages/"${project_name}"/*.nupkg --source "${sourceName}" --skip-duplicate
 else 
      echo "API key has been provided, it will be used for 'nuget push'"
-    dotnet nuget push nuget-packages/"${project_name}"/*.nupkg --api-key ${apiKey} --source ${sourceName} --skip-duplicate
+    dotnet nuget push nuget-packages/"${project_name}"/*.nupkg --api-key "${apiKey}" --source "${sourceName}" --skip-duplicate
 fi
 
